f5811a54095f1bce79b144ddeec3e718b4096dc6
Gregory Mitrano <gregory.mitrano@gmail.com>
2019-12-12 00:57:57 -0500

    Refactored Memory Interface
    The new interface allows for HW based memory.

--- hw/top.v
+++ hw/top.v
@@ -5,32 +5,29 @@ module top
 (
     input i_clk,
     input i_rst,
-
-    input      [1:0]  i_reg_ctl,
-    input      [31:0] i_reg_addr,
-    input      [31:0] i_reg_data,
-    output reg [31:0] o_reg_data,
-
     input i_logic_en,
 
-    output reg o_read_en,
-    output reg [63:0] o_read_addr,
+    input  wire [1:0]  i_mem_op,
+    input  wire [63:0] i_mem_addr,
+    input  wire [63:0] i_mem_data,
+    output reg  [63:0] o_mem_data,
 
-    output reg o_write_en,
-    output reg [63:0] o_write_addr,
-
-    input wire [63:0] i_data,
-    output reg [63:0] o_data
+    output wire o_mem_op_pending
 );
 
-parameter CTL_NOP   = 2'h0;
-parameter CTL_READ  = 2'h1;
-parameter CTL_WRITE = 2'h2;
+parameter MEM_OP_NOP   = 2'h0;
+parameter MEM_OP_READ  = 2'h1;
+parameter MEM_OP_WRITE = 2'h2;
 
-wire [1:0]  w_mem_op[3:0];
-wire [12:0] w_mem_addr[3:0];
-wire [63:0] w_mem_data_in[3:0];
-reg  [63:0] w_mem_data_out[3:0];
+parameter MEM_ADDR_HALTED = 64'h8000_0000_0000_0000;
+
+reg [1:0]  w_mem_op[3:0];
+reg [12:0] w_mem_addr[3:0];
+reg [63:0] w_mem_data_in[3:0];
+reg [63:0] r_mem_data_out[3:0];
+reg        r_mem_op_pending;
+
+assign o_mem_op_pending = r_mem_op_pending;
 
 mem mem_inst
 (
@@ -40,7 +37,7 @@ mem mem_inst
     .i_op(w_mem_op),
     .i_addr(w_mem_addr),
     .i_data(w_mem_data_in),
-    .o_data(w_mem_data_out)
+    .o_data(r_mem_data_out)
 );
 
 wire w_halted;
@@ -52,16 +49,45 @@ icp icp_inst
 
     .o_op(w_mem_op),
     .o_addr(w_mem_addr),
-    .i_data(w_mem_data_out),
+    .i_data(r_mem_data_out),
     .o_data(w_mem_data_in),
 
     .o_halted(w_halted)
 );
 
 always @ (posedge i_clk)
-    begin
-        if ((i_reg_ctl == CTL_READ) & (i_reg_addr == 0))
-            o_reg_data <= {{31{1'd0}}, w_halted};
-    end
+    if (i_rst)
+        r_mem_op_pending <= 0;
+    else
+        if (r_mem_op_pending)
+            begin
+                o_mem_data <= r_mem_data_out[0];
+                r_mem_op_pending <= 0;
+            end
+        else
+            begin
+                if (i_mem_addr[63])
+                    // If the high bit is set, this is a register operation
+                    begin
+                        case (i_mem_addr)
+                            MEM_ADDR_HALTED:
+                                if (i_mem_op == MEM_OP_READ)
+                                    o_mem_data <= {{63{1'd0}}, w_halted};
+                        endcase
+                    end
+                else
+                    // This is a regular memory operation
+                    begin
+                        // Forward the memory operation to the memory unit
+                        w_mem_op[0]   <= i_mem_op;
+                        w_mem_addr[0] <= i_mem_addr[12:0];
+
+                        if (i_mem_op == MEM_OP_READ)
+                            // Memory reads have a one cycle latency so we need to wait here
+                            r_mem_op_pending <= 1;
+                        else if (i_mem_op == MEM_OP_WRITE)
+                            w_mem_data_in[0] <= i_mem_data;
+                    end
+            end
 
 endmodule
--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,48 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+size_t MakeRegAddr(size_t regAddr)
+{
+    return (regAddr | 0x8000000000000000ull);
+}
+
 int main(int argc, char** argv)
 {
-    uint32_t pMemory[] =
+    uint64_t pMemory[] =
     {
         1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,2,23,10,27,1,6,27,31,1,31,6,35,2,35,10,39,1,39,5,43,2,6,43,47,2,47,10,51,1,51,6,55,1,55,6,59,1,9,59,63,1,63,9,67,1,67,6,71,2,71,13,75,1,75,5,79,1,79,9,83,2,6,83,87,1,87,5,91,2,6,91,95,1,95,9,99,2,6,99,103,1,5,103,107,1,6,107,111,1,111,10,115,2,115,13,119,1,119,6,123,1,123,2,127,1,127,5,0,99,2,14,0,0
     };
 
+    const size_t kNumQwords = static_cast<size_t>(sizeof(pMemory) / sizeof(pMemory[0]));
+
     ProtoBridge hBridge;
-    CreateProtoBridge(&hBridge, &pMemory, sizeof(pMemory));
+    CreateProtoBridge(&hBridge);
+
+    // Upload initial memory
+    WriteProtoBridgeMemory(hBridge, pMemory, sizeof(pMemory), 0);
 
     uint32_t numCycles = 0;
-    while (ReadProtoBridgeRegister(hBridge, 0) == 0)
+    uint64_t isHalted = 0;
+
+    do
     {
-        UpdateProtoBridge(hBridge);
+        ClockProtoBridge(hBridge);
         ++numCycles;
-    }
 
-    printf("Finished in %u Cycles\n", numCycles);
+        ReadProtoBridgeMemory(hBridge, MakeRegAddr(0), sizeof(isHalted), &isHalted);
+    } while (isHalted == 0);
 
-    const uint32_t kNumDwords = static_cast<uint32_t>(sizeof(pMemory) / sizeof(pMemory[0]));
-    printf("Final Memory[%u]: {\n", kNumDwords);
+    // Read back final memory
+    ReadProtoBridgeMemory(hBridge, 0, sizeof(pMemory), pMemory);
 
-    for (uint32_t dwordIndex = 0; dwordIndex < (sizeof(pMemory) / sizeof(pMemory[0])); ++dwordIndex)
+    // Subtract a single cycle because it takes a cycle to detect the halted state change
+    printf("Finished in %u Cycles\n", numCycles - 1);
+
+    printf("Final Memory[%u]: {\n", static_cast<uint32_t>(kNumQwords));
+
+    for (size_t qwordIndex = 0; qwordIndex < kNumQwords; ++qwordIndex)
     {
-        printf("%u,", pMemory[dwordIndex]);
+        printf("%" PRIu64 ",", pMemory[qwordIndex]);
     }
     printf("\n}\n");
 
